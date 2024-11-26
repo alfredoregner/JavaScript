d50f8d13a54c5b4591441bbe41a91e79606b6462
Alfredo Regner <94856772+alfredoregner@users.noreply.github.com>
2024-11-25 22:00:45 -0300

    Primeiros exemplos de Document Object Model

---
+++ documentObjectModel/exemplo1_alfredo/appendChild_alfredo.js
@@ -0,0 +1,15 @@
+// Seleciona o título e altera seu texto
+const titulo = document.getElementById("titulo");
+titulo.innerText = "DOM em Ação!";
+
+// Seleciona a lista e adiciona um novo item
+const lista = document.querySelector("ul");
+const novoItem = document.createElement("li");
+novoItem.innerText = "Item 3";
+lista.appendChild(novoItem);
+
+const nome = document.createElement("li");
+nome.innerText = "Alfredo";
+lista.appendChild(nome);
+
+lista.removeChild(novoItem)
